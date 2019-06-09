4a20aedbce848b1a87581d0658ce77c2cb9cf718
travisredd <travisredd@mail.weber.edu>
2019-06-09 14:09:42 -0600

    Add project files.

---
+++ GraniteWarehouse/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GraniteWarehouse.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraniteWarehouse.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        DbSet<ProductTypes> ProductTypes { get; set; }
+    }
+}
